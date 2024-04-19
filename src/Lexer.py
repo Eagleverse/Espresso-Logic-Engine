008be31195db84d508a5f5cfb09ea87ba1fec69e
James Vo <48601146+Eagleverse@users.noreply.github.com>
2024-04-18 20:04:40 -0500

    lexer updates

--- src/Lexer.py
+++ src/Lexer.py
@@ -2,30 +2,63 @@ import re
 from Token import Token
 
 class Lexer:
-    
-    @staticmethod
+    @staticmethod #staticmethod
+    # parse all the things
     def lex(text):
+        # while thing is true
+        # ruh oh thing is bad 
+        # Quit, boot user to start
+        # goals
+        # match is good 👍
+
+        # boolean
+        valid_session = True
+        # We're looking for an excuse to make this false and boot
+        # the user out. Extra secure 🤌🤌🤌
         tokens = []
-        
-        for line in text.split('\n'):
-            # Check if the line is empty
-            if line:
-                last_pos = 0
-        
-                # Search for name at start of line and add it as a token
-                match = re.match(r'^[A-Z][a-z]+ [A-Z][a-z]+', line)
-                if match:
-                    name = match.group(0)
-                    tokens.append(Token(Token.NAME, name, 0))
-                    last_pos = match.end()
-                    print("Success")
-                
-                else:
-                    print("Line must begin with a name")
-        
-        return tokens
+        # start y'alls loop
+
+        while(valid_session):
+            for line in text.split('\n'): # mel brown dep 5 bucks in acc
+                # Check if the line is empty
+                if line:
+                    last_pos = 0
+            
+                    # Search for name at start of line and add it as a token
+                    match = re.match(r'^[A-Z][a-z]+ [A-Z][a-z]+', line)
+                    if match: 
+                        name = match.group(0)
+                        # What does this do?
+                        tokens.append(Token(Token.NAME, name, 0))
+                        # Explain
+                        last_pos = match.end()
+                        print("Success")
+                        # Warning, could be a bad time.
+                    else:
+                        print("Line must begin with a name")
+                        valid_session = False # terminate loop 🪦 
+                    # search action
+                    # trim what was name
+                    # ["Name","Action","yada yada"]
+                    line = line[last_pos:].strip()
+                    print(line)
+                    match = re.match(r'(withdraw)|(deposit)|(create)',line)
+                    if match:
+                        action = match.group(0)
+                        # What does this do?
+                        tokens.append(Token(Token.NAME, name, 0))
+                        # Explain
+                        last_pos = match.end()
+                        print("Success")
+                        # Warning, could be a bad time.
+                    else:
+                        print("action is bad")
+                        valid_session = False # terminate loop 🪦 
+
+        return tokens # I'm outside whiel loop
+
     
-    lex()
+    lex("James Vooo deposit")
     
     
         
--- src/Token.py
+++ src/Token.py
@@ -3,12 +3,25 @@
 
 class Token:
     #TOKEN TYPES
-    NAME = "Name"
-    
+    NAME = "Name" 
+    ACTION = "Action"
+    ACCOUNT = "Account"
+    # :)
+    # lil function to print tokens
+    # Next = profit
+
     #Initializer
     def __init__(self, type, value, column):
         self.type = type
         self.value = value
         self.column = column
+    
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+    # If exists return the things
+    # token token object object ??? lol
+    # S T E A L
     
     
