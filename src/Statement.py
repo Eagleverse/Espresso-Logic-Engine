91bb0f924c55f88b9f814c603dd836ccfd8da4ed
brownm26csp <brownm26@csp.edu>
2024-04-25 20:28:57 -0500

    more changes to code

--- src/Statement.py
+++ src/Statement.py
@@ -5,4 +5,23 @@ class Statement:
         self.Action = None
         self.Amount = None
         self.Account = None
-        
+
+    def evaluate(self, accounts):
+        curAccount = next((x for x in accounts if x.accountID == self.Account), None)
+        print(curAccount)
+        if curAccount == None:
+            raise ValueError("Account does not exist")
+        elif self.Action == "withdraw":
+            if self.Name != (curAccount.firstName + " " + curAccount.lastName):
+                raise ValueError("Not authorized")
+            else:
+                self.Amount = self.Amount[1:]
+                curAccount.withdraw(float(self.Amount))
+                print(curAccount.getBalance())
+        elif self.Action == "deposit":
+            if self.Name != (curAccount.firstName + " " + curAccount.lastName):
+                raise ValueError("Not authorized")
+            else:
+                self.Amount = self.Amount[1:]
+                curAccount.deposit(float(self.Amount))
+                print(curAccount.getBalance())
--- src/espresso.py
+++ src/espresso.py
@@ -1,1 +1,19 @@
-print("hello everyone!")
+'''
+Names
+
+CSC 330 Language Design and Implementation
+
+(description)
+
+Code is our own
+'''
+
+import Bank
+import Lexer
+import Program
+
+
+bank = input("What bank action would you like to do?")
+print(bank)
+accessibleAccounts = Bank.Bank.initializeAccounts()
+Program.Program.parse(Lexer.Lexer.lex(bank)).Statement.evaluate(accessibleAccounts)
