6eebbf8469aaf0db51a0012703c5f006f301a2e7
brownm26csp <brownm26@csp.edu>
2024-04-27 12:28:37 -0500

    Added comments
    Removed unused variables, print statements, and comments

--- src/Lexer.py
+++ src/Lexer.py
@@ -1,36 +1,75 @@
+'''
+Brandon Pranke
+Melissa Brown
+
+CSC 330 Language Design and Implementation
+
+4/28/2024
+
+Week 7: Final Project
+
+Lexer class which takes in a line of code to find and return the tokens within
+
+We certify that this is our own original work
+
+'''
+
 import re
 from Token import Token
 
 
 class Lexer:
-    @staticmethod #staticmethod
-    # parse all the things
+    @staticmethod 
     def lex(text):
+
+        # array to hold all found tokens
         tokens = []
         for line in text.split('\n'):
             if not line:
-                continue  # Skip empty lines
+                continue
+
+            # keeps track of where in the line we have already checked for tokens
             last_pos = 0
             
+            # Checks for a name
+            # Two words, each must begin with capital letter, only letters allowed
+            # Valid: Melissa Brown, Brandon Pranke
+            # Invalid: Melissa, brandon pranke
             match = re.match(r'^[A-Z][a-z]+ [A-Z][a-z]+', line)
+
             if match:
+                # value is retrieved from match
                 name = match.group(0)
+                # token object is created with "name" value and added to tokens array
                 tokens.append(Token(Token.NAME, name, last_pos))
+                # position tracker is moved to the end of the match so that the rest of the line can be checked
                 last_pos = match.end()
             else:
+                # program is terminated if name is not correctly formatted
                 raise ValueError("Line must begin with a name")
 
+            # The part of the line which was already checked is removed from the string
             line = line[last_pos:].strip()
             
-            match = re.match(r'^(withdraw|deposit|create)', line)
+            # checks for a valid action
+            # must be 'withdraw' or 'deposit'
+            # Valid: "Melissa Brown withdraw..", "Brandon Pranke despoit..."
+            # Invalid: "Peter Parker steal..."
+            match = re.match(r'^(withdraw|deposit)', line)
             if match:
+                # value is retrieved from match
                 action = match.group(0)
+                # token object is created with "action" value and added to tokens array
                 tokens.append(Token(Token.ACTION, action, last_pos))
                 last_pos = match.end()
             else:
                 raise ValueError("Action is invalid. There must be an action")
             line = line[last_pos:].strip()
             
+            # checks for valid dollar amount
+            # Must being with dollar sign ($), have a decimal point, and 2 numbers after
+            # Valid: ".... $1.12",".... $5000.45"
+            # Invalid: "... 1", ".... 145.56"
             match = re.match(r'^(\$\d+\.\d{2})', line)
             if match:
                 amount = match.group(0)
@@ -40,7 +79,10 @@ class Lexer:
                 raise ValueError("Amount is invalid. There must be an amount")
             line = line[last_pos:].strip()
             
-            
+            # Checks for valid account number
+            # Must begin with 2 capital letters and be followed by 6 digits
+            # Valid: "...MB123456", "CC567890"
+            # Invalid: "... mb123456", "...CD123"
             match = re.match(r'^[A-Z]{2}\d{6}$', line)
             if match:
                 account = match.group(0)
@@ -49,11 +91,7 @@ class Lexer:
             else:
                 raise ValueError("Account number is invalid. line must end with Account number ")
 
+        # returns array with discovered tokens if no errors were raised
         return tokens
-        # return tokens
-
-    
-    # myTokens = lex("Melissa Brown withdraw $150.00 MB123456")
-    # print(myTokens)
     
         
